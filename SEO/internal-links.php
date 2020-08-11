c3ba0fc29205b55cba0cfa43cc4e1f4275064991
sunda <sundamrt@gmail.com>
2020-08-11 11:58:47 +0200

    Format internal-links.php

--- SEO/internal-links.php
+++ SEO/internal-links.php
@@ -1,12 +1,14 @@
+<?php
 /** Change keywords to links */
 
-function link_words( $text ) {
-$replace = array(
-'seo' => '<a href="https://itzen.hu/keresooptimalizalas-elmelete/">Keresőoptimalizálás</a>',
+function link_words($text)
+{
+    $replace = [
+        'seo' => '<a href="https://itzen.hu/keresooptimalizalas-elmelete/">Keresőoptimalizálás</a>',
+    ];
 
-);
-$text = str_replace( array_keys($replace), $replace, $text );
-return $text;
+    $text = str_replace(array_keys($replace), $replace, $text);
+    return $text;
 }
-add_filter( 'the_content', 'link_words' );
-add_filter( 'the_excerpt', 'link_words' );
+add_filter('the_content', 'link_words');
+add_filter('the_excerpt', 'link_words');
